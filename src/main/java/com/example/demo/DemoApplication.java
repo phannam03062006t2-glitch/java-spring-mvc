91067117cf8654747247ba777fbb86726bdabe78
Phan Duy Nam <phannam03062006t2@gmail.com>
2026-06-29 16:54:30 +0700

    add user

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import org.apache.catalina.core.ApplicationContext;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +9,7 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+
 	}
 
 }
--- src/main/java/com/example/demo/HelloWorld.java
+++ src/main/java/com/example/demo/HelloWorld.java
@@ -2,12 +2,19 @@ package com.example.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 public class HelloWorld {
 
     @GetMapping("/")
     public String hello() {
-        return "Hello World from Nem";
+        return "Hello World from mmmmmmm";
     }
+
+    @GetMapping("/user")
+    public String user() {
+        return "user";
+    }
+
 }
